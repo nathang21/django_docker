3109e580fd8c18a2f69b51ae74f1e39972804bc8
Nathan Guenther <nathanpguenther@gmail.com>
2016-09-18 05:52:02 +0000

    Fix compatibility for Django 1.10

--- django_docker/django_docker/urls.py
+++ django_docker/django_docker/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import include, url
+from hello_world.views import hello_world
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'hello_world.views.hello_world', name='hello_world'),
-)
+    url(r'^$', hello_world, name='home'),
+    #url(r'^$', 'hello_world.views.hello_world', name='hello_world'),
+]
